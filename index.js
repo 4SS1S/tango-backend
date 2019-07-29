0bf1df1ef9267b758ba71bbefd9fd411d82f3c86
Assis <assisguitar23@gmail.com>
2019-07-29 00:15:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const mongoose = require('mongoose');
 
+const f = require('util').format;
+const fs = require('fs');
+
+
+var ca = [fs.readFileSync(__dirname + "/ssl/ca.pem")];
+var cert = fs.readFileSync(__dirname + "/ssl/client.pem");
+var key = fs.readFileSync(__dirname + "/ssl/client.pem");
+
 require("dotenv").config();
 
 const app = require("./app");
 
 
 mongoose.connect(process.env.URL_MONGODB, {
-    useNewUrlParser:true,
+  server: {
+      sslValidate:true
+    , sslCA:ca
+    , sslKey:key
+    , sslCert:cert
+    , sslPass:'10gen'
+  }
+}, function(err, db) {
+  db.close();
 });
 
 app.listen(3333);
